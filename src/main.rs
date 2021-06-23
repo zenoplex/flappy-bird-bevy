b223f0b0a66986cdc0f8a21f9b763ba382b78e71
Gen Ichino <zenoplex@gmail.com>
2021-06-24 01:30:14 +0900

    feat: flap

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use bevy::prelude::*;
 
+const TIME_STEP: f32 = 1.0 / 60.0;
+const GRAVITY: f32 = 5.0;
+
 #[derive(Debug)]
-struct Player {}
+struct Player {
+    velocity: Vec3,
+}
 
 #[derive(Debug)]
 struct Pipe {}
@@ -12,11 +17,7 @@ struct Position {
     y: f32,
 }
 
-#[derive(Debug)]
-struct Velocity {
-    x: f32,
-    y: f32,
-}
+struct GameTimer(Timer);
 
 fn add_player(
     asset_server: Res<AssetServer>,
@@ -31,14 +32,14 @@ fn add_player(
             // sprite: Sprite::new(Vec2::new(120.0, 30.0)),
             ..Default::default()
         })
-        .insert_bundle((Player {}, Velocity { x: 0.0, y: 1.0 }));
+        .insert(Player {
+            velocity: Vec3::ZERO,
+        });
 }
 
 fn add_pipes(mut commands: Commands) {
     commands.spawn_bundle((Pipe {}, Position { x: 10.0, y: 0.0 }));
 }
-
-struct GameTimer(Timer);
 
 fn hello(time: Res<Time>, mut timer: ResMut<GameTimer>, query: Query<&Position, With<Player>>) {
     if timer.0.tick(time.delta()).just_finished() {
@@ -52,36 +53,53 @@ fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
-fn input_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut Transform)>) {
-    if let Ok((_, mut transform)) = query.single_mut() {
-        let mut direction = 0.0;
+fn input_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Player, &mut Transform)>,
+) {
+    if let Ok((mut player, mut transform)) = query.single_mut() {
         if keyboard_input.pressed(KeyCode::Up) {
-            direction -= 1.0;
+            player.velocity.y = 5.0;
         }
 
-        let translation = &mut transform.translation;
-        translation.y += direction;
-        translation.y = translation.y.min(100.0).max(-200.0);
+        // let translation = &mut transform.translation;
+        // translation.y += direction;
+        // translation.y = translation.y.min(100.0).max(-200.0);
     }
 }
 
-fn move_system(time: Res<Time>, mut q: Query<(&Player, &mut Transform)>) {
-    let delta = time.delta_seconds();
+fn move_system(
+    time: Res<Time>,
+    mut timer: ResMut<GameTimer>,
+    mut q: Query<(&mut Player, &mut Transform)>,
+) {
+    if !timer.0.tick(time.delta()).just_finished() {
+        return;
+    };
 
-    for (v, mut t) in q.iter_mut() {
-        t.translation.y += delta * 10.0;
-        // t.rotate(Quat::from_rotation_z(v.rotation * delta));
-        println!("{:?}", t.translation.y);
+    // if !timer.0.finished() {
+    //     println!("not yet");
+    //     return;
+    // }
+
+    for (mut player, mut t) in q.iter_mut() {
+        player.velocity.y -= GRAVITY * time.delta_seconds();
+        t.translation.y += player.velocity.y;
     }
+}
+
+fn game_timer_system(time: Res<Time>, mut timer: ResMut<GameTimer>) {
+    timer.0.tick(time.delta());
 }
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .insert_resource(GameTimer(Timer::from_seconds(5.0, true)))
+        // Maybe try fixed time step?
+        .insert_resource(GameTimer(Timer::from_seconds(TIME_STEP, true)))
         .add_startup_system(setup.system())
         .add_startup_system(add_player.system())
-        // .add_startup_system(add_pipes.system())
+        .add_startup_system(add_pipes.system())
         .add_system(hello.system())
         .add_system(move_system.system())
         .add_system(input_system.system())
