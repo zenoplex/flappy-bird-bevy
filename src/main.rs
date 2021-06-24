83438a17b5925ef0e2ee235fdc4141d769eecf4f
Gen Ichino <zenoplex@gmail.com>
2021-06-24 22:09:37 +0900

    refactor: make gravity framerate independent

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
 
-const TIME_STEP: f32 = 1.0 / 60.0;
-const GRAVITY: f32 = 5.0;
+const GRAVITY: f32 = 10.0;
 
 #[derive(Debug)]
 struct Player {
@@ -16,8 +15,6 @@ struct Position {
     x: f32,
     y: f32,
 }
-
-struct GameTimer(Timer);
 
 fn add_player(
     asset_server: Res<AssetServer>,
@@ -41,14 +38,6 @@ fn add_pipes(mut commands: Commands) {
     commands.spawn_bundle((Pipe {}, Position { x: 10.0, y: 0.0 }));
 }
 
-fn hello(time: Res<Time>, mut timer: ResMut<GameTimer>, query: Query<&Position, With<Player>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for player in query.iter() {
-            println!("{:?}", player);
-        }
-    }
-}
-
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
@@ -61,46 +50,22 @@ fn input_system(
         if keyboard_input.pressed(KeyCode::Up) {
             player.velocity.y = 5.0;
         }
-
-        // let translation = &mut transform.translation;
-        // translation.y += direction;
-        // translation.y = translation.y.min(100.0).max(-200.0);
     }
 }
 
-fn move_system(
-    time: Res<Time>,
-    mut timer: ResMut<GameTimer>,
-    mut q: Query<(&mut Player, &mut Transform)>,
-) {
-    if !timer.0.tick(time.delta()).just_finished() {
-        return;
-    };
-
-    // if !timer.0.finished() {
-    //     println!("not yet");
-    //     return;
-    // }
-
+fn move_system(time: Res<Time>, mut q: Query<(&mut Player, &mut Transform)>) {
     for (mut player, mut t) in q.iter_mut() {
-        player.velocity.y -= GRAVITY * time.delta_seconds();
+        player.velocity.y += -GRAVITY * time.delta_seconds();
         t.translation.y += player.velocity.y;
     }
-}
-
-fn game_timer_system(time: Res<Time>, mut timer: ResMut<GameTimer>) {
-    timer.0.tick(time.delta());
 }
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        // Maybe try fixed time step?
-        .insert_resource(GameTimer(Timer::from_seconds(TIME_STEP, true)))
         .add_startup_system(setup.system())
         .add_startup_system(add_player.system())
         .add_startup_system(add_pipes.system())
-        .add_system(hello.system())
         .add_system(move_system.system())
         .add_system(input_system.system())
         .run();
